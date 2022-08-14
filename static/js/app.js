2749a73e979aa17e24044372297fe282a257c426
truthprice <truthop@gmail.com>
2022-08-14 19:54:47 -0400

    loaded names into the dropdown list

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,15 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// Use d3 to load the data and select the names for the dropdown list. Structure take from--> https://www.geeksforgeeks.org/d3-js-selection-enter-function/
 d3.json(url).then(function(data) {
-    console.log(data);
+    d3.select("#selDataset")
+    .selectAll("option")
+    .data(data.names)
+    .enter()
+    .append("option")
+    .text((data) => {
+        return data;
+    });
     
 });
+
