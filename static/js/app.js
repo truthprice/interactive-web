b146fd6e8953d144a9fc4f78c37e0c70012bfff4
truthprice <truthop@gmail.com>
2022-08-18 09:38:48 -0400

    updated app.py with graphs and demo_info

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,6 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+const url = "./samples.json";
 
-// Use d3 to load the data and select the names for the dropdown list. Structure take from--> https://www.geeksforgeeks.org/d3-js-selection-enter-function/
+// Use d3 to load the data and select the test subject for the dropdown list. Structure take from--> https://www.geeksforgeeks.org/d3-js-selection-enter-function/
 d3.json(url).then(function(data) {
     d3.select("#selDataset")
     .selectAll("option")
@@ -12,4 +12,93 @@ d3.json(url).then(function(data) {
     });
     
 });
+
+// function init() {
+//     let data = [{
+//       values: [],
+//       labels: [],
+//       type: "bar"
+//     }];
+  
+//     let layout = {
+//       height: 600,
+//       width: 800
+//     };
+  
+//     Plotly.newPlot("bar", data, layout);
+// }
+//Function created to locate data when a test subject is chosen from the dropdown
+
+function optionChanged(value) {
+    d3.json(url).then(function(data){
+        // console.log(data);
+        var samplesData = data.samples;
+        // console.log(samplesData);
+        var sample_values = [];
+        var otu_ids = [];
+        var otu_labels = [];
+        var otuIdString =[];
+        samplesData.forEach(subject => {
+            if (subject.id == value) {
+                sample_values = subject.sample_values;
+                otu_ids = subject.otu_ids;
+                otu_labels = subject.otu_labels;
+                otu_ids.map(otu =>{
+                    otuIdString.push(`OTU ${otu}`);
+                });
+            }
+        });
+
+
+    var trace = {
+        x: sample_values.slice(0, 10).reverse(),
+        y: otuIdString.slice(0, 10).reverse(),
+        text: otu_labels.slice(0, 10).reverse(),
+        orientation: "h",
+        type: 'bar'
+    }
+    
+
+    Plotly.newPlot('bar', [trace]);
+    
+    var trace2 = {
+        x: otu_ids,
+        y: sample_values,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+            size: sample_values,
+            color: otu_ids
+        }
+    }
+   
+    var layout = {
+        xaxis: {
+            title: "OTU IDs"
+        },
+        title: "Bacteria Levels"
+    }
+
+    Plotly.newPlot('bubble', [trace2], layout);
+
+    var metaData=data.metadata;
+    metaData.forEach(subject => {
+        if (subject.id ==value){
+            var dem_info = Object.entries(subject);
+            d3.selectAll('p').remove();
+            d3.select('#sample-metadata')
+            .selectAll('p')
+            .data(dem_info)
+            .enter()
+            .append('p')
+            .text(demographics=>{
+                return `${demographics[0]}: ${demographics[1]}`;
+            });
+        }
+    });
+
+    });
+
+}
+// init();
 
